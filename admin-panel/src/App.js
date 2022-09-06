e43b89fda7c66a7c5343eff7e9822e646280a887
Mubson1 <flammablekarki@gmail.com>
2022-09-06 13:16:04 +0545

    Final commit

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,5 +1,5 @@
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+import Home from "./pages/home/Home"
+import Login from "./pages/login/Login"
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
